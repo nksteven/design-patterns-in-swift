30ca1961c128003ab8f20ebb6721bc66fdc9179c
Yang Shaoli <yangshaoli2005@gmail.com>
2017-03-29 18:44:39 +0800

    add command line project sample

---
+++ MyCommandLine/MyCommandLine/MyCode.swift
@@ -0,0 +1,19 @@
+//
+//  MyCode.swift
+//  MyCommandLine
+//
+//  Created by Yang Shaoli on 17/03/2017.
+//  Copyright © 2017 Yang Shaoli. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+    func writeHello() {
+        print("Hello!")
+    }
+    
+    private func writePassword() {
+        print("Secret")
+    }
+}
---
+++ MyCommandLine/MyCommandLine/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  MyCommandLine
+//
+//  Created by Yang Shaoli on 17/03/2017.
+//  Copyright © 2017 Yang Shaoli. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let myObject = MyClass()
+myObject.writeHello()
